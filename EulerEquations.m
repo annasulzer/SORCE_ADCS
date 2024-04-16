4fcc19105ca835e92046471a1b7257edd3c57079
annasulzer <144078462+annasulzer@users.noreply.github.com>
2024-04-16 12:36:15 -0700

    added some plots

--- EulerEquations.m
+++ EulerEquations.m
@@ -5,13 +5,30 @@
 clear; clc;
 
 %Initial conditions
-omega_init = [0; 1; 0.001];
-rps = norm(omega_init)/2*pi; %rotations per second
-
+omega_init = [0.05; 0.05; 0.05];
+T = 2*pi/norm(omega_init);
 %Integrate
 options = odeset('RelTol', 1e-3, 'AbsTol', 1e-6);
-tstart = 0; tint = 0.01; tend = 1000/rps;
+tstart = 0; tint = 0.1; tend = 1*T/tint;
 [t_out, omega_out] = ode113(@omega_dot,[tstart:tint:tend]', omega_init, options);
+t_out = t_out * tint; %to make seconds
+
+
+% Plot
+figure()
+subplot(3,1,1)
+plot(t_out, omega_out(:, 1))
+xlabel('t [s]')
+ylabel('\omega_x [rad/s^2]')
+title('Angular Velocity over time')
+subplot(3,1,2)
+plot(t_out, omega_out(:, 2))
+xlabel('t [s]')
+ylabel('\omega_y [rad/s^2]')
+subplot(3,1,3)
+plot(t_out, omega_out(:, 3))
+xlabel('t [s]')
+ylabel('\omega_z [rad/s^2]')
 
 %% Ellipsoids
 inertia_p = diag([85.075, 110.796, 120.2515]); %inertia in principal axes
@@ -25,19 +42,44 @@ L =  sqrt(omega_init(1)^2*inertia_p(1,1)^2 + omega_init(2)^2*inertia_p(2,2)^2 + 
 [x_momE, y_momE, z_momE] = ellipsoid(0,0,0, sqrt(L^2/inertia_p(1,1)^2), sqrt(L^2/inertia_p(2,2)^2), sqrt(L^2/inertia_p(3,3)^2)); %energy ellipsoid
 
 
-% %%
-% figure()
-% hold on;
-% fsurf(@(omx,omy)  sqrt(2*T/inertia_p(3,3)*(1 - omx^2/(2*T/inertia_p(1,1)) - omy^2/(2*T/inertia_p(2,2)))), 'FaceColor', 'blue', 'EdgeColor', 'blue', 'FaceAlpha',.2)
-% fsurf(@(omx,omy) -sqrt(2*T/inertia_p(3,3)*(1 - omx^2/(2*T/inertia_p(1,1)) - omy^2/(2*T/inertia_p(2,2)))), 'FaceColor', 'blue', 'EdgeColor', 'blue', 'FaceAlpha',.2)
-% axis equal;
-% 
-% fsurf(@(omx,omy)  sqrt(L^2/inertia_p(3,3)^2*(1 - omx^2/(L^2/inertia_p(1,1)^2) - omy^2/(L^2/inertia_p(2,2)^2))), [-norm(omega_init) norm(omega_init) -norm(omega_init) norm(omega_init)], 'FaceColor', 'red', 'EdgeColor', 'red', 'FaceAlpha',.2)
-% fsurf(@(omx,omy) -sqrt(L^2/inertia_p(3,3)^2*(1 - omx^2/(L^2/inertia_p(1,1)^2) - omy^2/(L^2/inertia_p(2,2)^2))), [-norm(omega_init) norm(omega_init) -norm(omega_init) norm(omega_init)], 'FaceColor', 'red', 'EdgeColor', 'red', 'FaceAlpha',.2)
-% xlabel('\omega_x [1/s^2]')
-% ylabel('\omega_y [1/s^2]')
-% zlabel('\omega_z [1/s^2]')
-% legend('Energy Ellipsoid','', 'Momentum Ellipsoid')
+%% Validate Ellipsoid
+%Energy
+a = sqrt(2*T/inertia_p(1,1));
+b = sqrt(2*T/inertia_p(2,2));
+c = sqrt(2*T/inertia_p(3,3));
+figure()
+surface(x_enE, y_enE, z_enE, 'FaceColor', 'blue', 'EdgeColor', 'black', 'FaceAlpha',.2, 'EdgeAlpha',.4);
+hold on;
+plot3(linspace(0,a), 0, 0,'.', 'Color','black', 'LineWidth', 2)
+plot3(0,linspace(0,b), 0, '.', 'Color','black', 'LineWidth', 2)
+plot3(0, 0, linspace(0,c),'.', 'Color','black', 'LineWidth', 2)
+view(3)
+xlabel('\omega_x [1/s^2]')
+ylabel('\omega_y [1/s^2]')
+zlabel('\omega_z [1/s^2]')
+grid on;
+axis equal;
+title('Energy Ellipsoid with semi-major axes')
+hold off;
+
+%Momentum
+a = sqrt(L^2/inertia_p(1,1)^2);
+b = sqrt(L^2/inertia_p(2,2)^2);
+c = sqrt(L^2/inertia_p(3,3)^2);
+figure()
+surface(x_momE, y_momE, z_momE, 'FaceColor', 'red', 'EdgeColor', 'black', 'FaceAlpha',.2, 'EdgeAlpha',.4);
+hold on;
+plot3(linspace(0,a), 0, 0,'.', 'Color','black', 'LineWidth', 2)
+plot3(0,linspace(0,b), 0, '.', 'Color','black', 'LineWidth', 2)
+plot3(0, 0, linspace(0,c),'.', 'Color','black', 'LineWidth', 2)
+view(3)
+grid on;
+axis equal;
+xlabel('\omega_x [1/s^2]')
+ylabel('\omega_y [1/s^2]')
+zlabel('\omega_z [1/s^2]')
+title('Momentum Ellipsoid with semi-major axes')
+
 %% Plot
 %Ellipsoid
 figure()
--- OrbitPropagation.m
+++ OrbitPropagation.m
@@ -6,26 +6,26 @@ clear; clc;
 % Initial conditions
 mu = 398600.435436; %Earth centered orbit
 rE = 6378;
-
+e = 0.0026520;
 om = 337.7127;%deg
 Om = 11.4426;%deg
 i = 39.9975; %deg
-%n = 14.80808427* 2*pi/86400; % mean motion in rad per sec
-%r = (mu/(n^2))^(1/3);
+n = 14.80808427* 2*pi/86400; % mean motion in rad per sec
+a = (mu/(n^2))^(1/3);
 
 %Perifocal
-r = 640 + rE;
-v = sqrt(mu/r); %circular orbit speed
+r = a*(1-e); %at periapsis
+v = sqrt(2*mu/r - mu/a); %circular orbit speed
 state_PQW_init = [r, 0, 0, 0, v, 0, 0]; %at periapsis
 
 %ECI
 R_PQW_IJK = rotz(Om)*rotx(i)*rotz(om); %rotation matrix from PQW to IJK, swapped sign bc active vs passive rotation
 state_ECI_init = [R_PQW_IJK*state_PQW_init(1:3)',R_PQW_IJK*state_PQW_init(4:6)'] ;
-T_orbit = 2*pi*sqrt(r^3/mu);
+T_orbit = 2*pi*sqrt(a^3/mu);
 
 %Orbit Propagator ode113
 options = odeset('RelTol', 1e-3, 'AbsTol', 1e-6);
-tstart = 0; tint = 0.01; tend = 1*T_orbit;
+tstart = 0; tint = 0.01; tend = 10*T_orbit;
 [t_out, state_out] = ode113(@state_dot,[tstart:tint:tend]', state_ECI_init, options);
 
 %% Plot
@@ -35,7 +35,7 @@ plot3(state_out(:, 1), state_out(:, 2), state_out(:, 3), 'red', 'LineWidth',2)
 %Earth
 opts.FaceAlpha = 0.6;
 opts.Units = 'km';
-opts.RotAngle = -127.1871; %figure out coordinate system of globe!!
+opts.RotAngle = -127.1871; 
 opts.RefPlane = 'ecliptic';
 planet3D('Earth',opts);
 axis equal;
@@ -45,7 +45,7 @@ grid on;
 xlabel('x [km]')
 ylabel('y [km]')
 zlabel('z [km]')
-title('Orbit around a Earth')
+title('Orbit around a Earth in ECI')
 hold off;
 
 %% return equations of motion
