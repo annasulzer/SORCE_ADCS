9886f3aca5bb9af85cd384cd070dcab2060db30d
annasulzer <144078462+annasulzer@users.noreply.github.com>
2024-04-29 20:29:06 -0700

    cleaned up simulink and implemented 1 and 2

--- EulerEquations.m
+++ EulerEquations.m
@@ -8,16 +8,16 @@ close all;
 %%
 %Initial conditions
 [state_ECI_init, T_orbit] = OrbitPropagation();
-omega_init = [0.05; 0.0; 0.1];
+omega_init = [0.0; 0.1; 0.0001];
 
 att_init = [0, 90, 0]; %3,1,3
 Rot = rotz(-att_init(3))*rotx(-att_init(2))*rotz(-att_init(1));
 
 %[R, T, N] = RTN_frame_inertial(state_ECI_init');
 
-%Rot = [R',T', N'];
+%Rot = [R',T', N']';
 
-DCM = Rot;
+DCM_initial = Rot;
 
 [R_princ,inertia_p] = inertia(); %inertia in principal axes
 %inertia_p = [85.075, 0, 0; 0, 85.075, 0; 0, 0, 120.2515]; %axial symmetric
@@ -26,7 +26,7 @@ DCM = Rot;
 absTol= 1e-10;
 relTol = 1e-6;
 %time = 2*pi/norm(omega_init);
-tstart = 0; tend = 0.1*T_orbit;
+tstart = 0; tend = 0.5*T_orbit;
 
 %% Simulate
 out = sim("main");
@@ -36,20 +36,18 @@ t_out = out.tout;
 omega_out = out.omega.Data(:,:)';
 L_out = (inertia_p * omega_out')';
 
-%attitude representation
-quat_out = out.quaternions.Data(:, :)';
+%attitude representation SWITCH
+%quat_out = out.quaternions.Data(:, :)';
 euler_out = out.euler.Data(:, :)';
-omega_inertial_euler_out =  out.omega_inertial_euler.Data(:, :)';
-omega_inertial_quat_out =  out.omega_inertial_quat.Data(:, :)';
-L_inertial_euler_out =  out.L_inertial_euler.Data(:, :)';
-L_inertial_quat_out =  out.L_inertial_quat.Data(:, :)';
-R_euler = out.R_Euler.Data(:,:,:);
-R_quat = out.R_Quat.Data(:,:);
+
+omega_out_inertial =  out.omega_inertial.Data(:, :)';
+L_out_inertial =  out.L_inertial.Data(:, :)';
+DCM_out = out.DCM.Data(:,:,:);
 
 %orbit propagation
 state_out = out.orbit_state.Data(:,:)';
 %% Get different coordinate frames with respect to inertial frame
-Rot = R_euler;
+Rot = DCM_out;
 [Xp, Yp, Zp, Xb, Yb, Zb] = principal_body_frame_inertial(Rot, R_princ);
 [R, T, N] = RTN_frame_inertial(state_out);
 
@@ -155,30 +153,126 @@ Rot = R_euler;
 
 %% Plot Attitude Representations PS3
 % Quaternions over time
-figure()
-subplot(4,1,1)
-hold on;
-plot(t_out, quat_out(:, 4), 'blue')
-xlabel('t [s]')
-ylabel('q4')
-title('Quaternions over time')
-subplot(4,1,2)
-hold on;
-plot(t_out, quat_out(:, 1), 'blue')
-xlabel('t [s]')
-ylabel('q1')
-subplot(4,1,3)
-hold on;
-plot(t_out, quat_out(:, 2), 'blue')
-xlabel('t [s]')
-ylabel('q2')
-subplot(4,1,4)
-hold on;
-plot(t_out, quat_out(:, 3), 'blue')
-xlabel('t [s]')
-ylabel('q3')
-
-% Euler Angles over time
+% figure()
+% subplot(4,1,1)
+% hold on;
+% plot(t_out, quat_out(:, 4), 'blue')
+% xlabel('t [s]')
+% ylabel('q4')
+% title('Quaternions over time')
+% subplot(4,1,2)
+% hold on;
+% plot(t_out, quat_out(:, 1), 'blue')
+% xlabel('t [s]')
+% ylabel('q1')
+% subplot(4,1,3)
+% hold on;
+% plot(t_out, quat_out(:, 2), 'blue')
+% xlabel('t [s]')
+% ylabel('q2')
+% subplot(4,1,4)
+% hold on;
+% plot(t_out, quat_out(:, 3), 'blue')
+% xlabel('t [s]')
+% ylabel('q3')
+% 
+% % Euler Angles over time
+% figure()
+% subplot(3,1,1)
+% hold on;
+% plot(t_out, rad2deg(euler_out(:, 1)), 'blue')
+% xlabel('t [s]')
+% ylabel('\phi [deg]')
+% title('Euler angles over time')
+% subplot(3,1,2)
+% hold on;
+% plot(t_out, rad2deg(euler_out(:, 2)), 'blue')
+% xlabel('t [s]')
+% ylabel('\theta [deg]')
+% subplot(3,1,3)
+% hold on;
+% plot(t_out, rad2deg(euler_out(:, 3)), 'blue')
+% xlabel('t [s]')
+% ylabel('\psi [deg]')
+% 
+% %% Check Attitude Representation
+% % Angular Momentum Vector
+% figure()
+% subplot(3,1,1)
+% hold on;
+% plot(t_out, L_inertial_euler_out(:, 1), 'blue')
+% plot(t_out, L_inertial_quat_out(:, 1), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('L_x [kg*m^2/s]')
+% title('Inertial angular momentum vector over time from the euler angles (blue) and quaternions (red)')
+% subplot(3,1,2)
+% hold on;
+% plot(t_out, L_inertial_euler_out(:, 2), 'blue')
+% plot(t_out, L_inertial_quat_out(:, 2), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('L_y [kg*m^2/s]')
+% subplot(3,1,3)
+% hold on;
+% plot(t_out, L_inertial_euler_out(:, 3), 'blue')
+% plot(t_out, L_inertial_quat_out(:, 3), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('L_z [kg*m^2/s]')
+% legend('Euler Angles', 'Quaternions')
+% 
+% %%
+% % Plot Omega over time
+% figure()
+% subplot(3,1,1)
+% hold on;
+% plot(t_out, omega_inertial_euler_out(:, 1), 'blue')
+% plot(t_out, omega_inertial_quat_out(:, 1), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('\omega_x [rad/s]')
+% title('Inertial angular velocity over time from the euler angles (blue) and quaternions (red)')
+% subplot(3,1,2)
+% hold on;
+% plot(t_out, omega_inertial_euler_out(:, 2), 'blue')
+% plot(t_out, omega_inertial_quat_out(:, 2), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('\omega_y [rad/s]')
+% subplot(3,1,3)
+% hold on;
+% plot(t_out, omega_inertial_euler_out(:, 3), 'blue')
+% plot(t_out, omega_inertial_quat_out(:, 3), '--', 'Color','red')
+% xlabel('t [s]')
+% ylabel('\omega_z [rad/s]')
+% legend('Euler Angles', 'Quaternions')
+% 
+% %Herpolhode Quaternions
+% figure()
+% hold on;
+% plot3(omega_inertial_quat_out(:,1), omega_inertial_quat_out(:,2), omega_inertial_quat_out(:, 3), 'Color', 'black', 'LineWidth', 1)
+% quiver3(0,0,0, L_inertial_quat_out(1,1)/50, L_inertial_quat_out(1,2)/50, L_inertial_quat_out(1,3)/50,'LineWidth', 1)
+% axis equal;
+% grid on;
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% legend('Herpolhode', 'Angular Momentum Vector (scaled)')
+% title('Herpolhode and Angular Momentum Vector based on Quaternions')
+% view(3)
+% 
+% %Herpolhode Euler
+% figure()
+% hold on;
+% plot3(omega_inertial_euler_out(:,1), omega_inertial_euler_out(:,2), omega_inertial_euler_out(:, 3), 'Color', 'black', 'LineWidth', 1)
+% quiver3(0,0,0, L_inertial_euler_out(1,1)/50, L_inertial_euler_out(1,2)/50, L_inertial_euler_out(1,3)/50,'LineWidth', 1)
+% axis equal;
+% grid on;
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% legend('Herpolhode', 'Angular Momentum Vector (scaled)')
+% title('Herpolhode and Angular Momentum Vector based on Euler Angles')
+% view(3)
+% 
+%% Plotting PSET4
+%Euler Angles over time
 figure()
 subplot(3,1,1)
 hold on;
@@ -197,79 +291,24 @@ plot(t_out, rad2deg(euler_out(:, 3)), 'blue')
 xlabel('t [s]')
 ylabel('\psi [deg]')
 
-%% Check Attitude Representation
-% Angular Momentum Vector
+%Plot Omega over time
 figure()
 subplot(3,1,1)
 hold on;
-plot(t_out, L_inertial_euler_out(:, 1), 'blue')
-plot(t_out, L_inertial_quat_out(:, 1), '--', 'Color','red')
-xlabel('t [s]')
-ylabel('L_x [kg*m^2/s]')
-title('Inertial angular momentum vector over time from the euler angles (blue) and quaternions (red)')
-subplot(3,1,2)
-hold on;
-plot(t_out, L_inertial_euler_out(:, 2), 'blue')
-plot(t_out, L_inertial_quat_out(:, 2), '--', 'Color','red')
-xlabel('t [s]')
-ylabel('L_y [kg*m^2/s]')
-subplot(3,1,3)
-hold on;
-plot(t_out, L_inertial_euler_out(:, 3), 'blue')
-plot(t_out, L_inertial_quat_out(:, 3), '--', 'Color','red')
-xlabel('t [s]')
-ylabel('L_z [kg*m^2/s]')
-legend('Euler Angles', 'Quaternions')
-
-%%
-% Plot Omega over time
-figure()
-subplot(3,1,1)
-hold on;
-plot(t_out, omega_inertial_euler_out(:, 1), 'blue')
-plot(t_out, omega_inertial_quat_out(:, 1), '--', 'Color','red')
+plot(t_out, omega_out_inertial(:, 1), 'blue')
 xlabel('t [s]')
 ylabel('\omega_x [rad/s]')
-title('Inertial angular velocity over time from the euler angles (blue) and quaternions (red)')
+title('Inertial angular velocity over time from the euler angles')
 subplot(3,1,2)
 hold on;
-plot(t_out, omega_inertial_euler_out(:, 2), 'blue')
-plot(t_out, omega_inertial_quat_out(:, 2), '--', 'Color','red')
+plot(t_out, omega_out_inertial(:, 2), 'blue')
 xlabel('t [s]')
 ylabel('\omega_y [rad/s]')
 subplot(3,1,3)
 hold on;
-plot(t_out, omega_inertial_euler_out(:, 3), 'blue')
-plot(t_out, omega_inertial_quat_out(:, 3), '--', 'Color','red')
+plot(t_out, omega_out_inertial(:, 3), 'blue')
 xlabel('t [s]')
 ylabel('\omega_z [rad/s]')
-legend('Euler Angles', 'Quaternions')
 
-%Herpolhode Quaternions
-figure()
-hold on;
-plot3(omega_inertial_quat_out(:,1), omega_inertial_quat_out(:,2), omega_inertial_quat_out(:, 3), 'Color', 'black', 'LineWidth', 1)
-quiver3(0,0,0, L_inertial_quat_out(1,1)/50, L_inertial_quat_out(1,2)/50, L_inertial_quat_out(1,3)/50,'LineWidth', 1)
-axis equal;
-grid on;
-xlabel('x')
-ylabel('y')
-zlabel('z')
-legend('Herpolhode', 'Angular Momentum Vector (scaled)')
-title('Herpolhode and Angular Momentum Vector based on Quaternions')
-view(3)
 
-%Herpolhode Euler
-figure()
-hold on;
-plot3(omega_inertial_euler_out(:,1), omega_inertial_euler_out(:,2), omega_inertial_euler_out(:, 3), 'Color', 'black', 'LineWidth', 1)
-quiver3(0,0,0, L_inertial_euler_out(1,1)/50, L_inertial_euler_out(1,2)/50, L_inertial_euler_out(1,3)/50,'LineWidth', 1)
-axis equal;
-grid on;
-xlabel('x')
-ylabel('y')
-zlabel('z')
-legend('Herpolhode', 'Angular Momentum Vector (scaled)')
-title('Herpolhode and Angular Momentum Vector based on Euler Angles')
-view(3)
 
