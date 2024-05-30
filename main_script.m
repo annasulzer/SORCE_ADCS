ceeabdaa283ff6697b52b39a60b8d77b3ebe0afc
annasulzer <144078462+annasulzer@users.noreply.github.com>
2024-05-29 21:03:23 -0700

    final version - USE main_ETHAN

--- main_script.m
+++ main_script.m
@@ -84,81 +84,106 @@ eclipse_condition = out.eclipse.Data();
 
 %% PSET8 Plotting
 %% Residuals Prefit
+%Stats
+mean_prefit_sun = mean(squeeze(prefit_res(:, 1, :)), 2)
+cov_prefit_sun = cov(squeeze(prefit_res(:, 1, :))')
+mean_prefit_mag = mean(squeeze(prefit_res(:, 2, :)), 2)
+cov_prefit_mag = cov(squeeze(prefit_res(:, 2, :))')
+mean_prefit_star = mean(squeeze(prefit_res(:, 3, :)), 2)
+cov_prefit_star = cov(squeeze(prefit_res(:, 3, :))')
+
 figure()
 subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(prefit_res(1, 1, :)), 'LineWidth',1)
+ylim([-0.05,0.05])
 xlabel('t [s]')
-ylabel('\Delta x')
-title('Prefit residuals (modelled vs actual) Sun Sensor (\sigma^2 = 0.5 deg)')
+ylabel('z_x')
+title('Prefit residuals Sun Sensor')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(prefit_res(2, 1, :)), 'LineWidth',1)
+ylim([-0.05,0.05])
 xlabel('t [s]')
-ylabel('\Delta y')
+ylabel('z_y')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(prefit_res(3, 1, :)), 'LineWidth',1)
+ylim([-0.05,0.05])
 xlabel('t [s]')
-ylabel('\Delta z')
+ylabel('z_z')
 
 %Magnet
 figure()
 subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(prefit_res(1, 2, :)), 'LineWidth',1)
+ylim([-0.0000001, 0.0000001])
 xlabel('t [s]')
-ylabel('\Delta x [T]')
-title('Prefit residuals (modelled vs actual) Magnetometer (\sigma^2 = 4.875*10^{-7} T)')
+ylabel('z_x [T]')
+title('Prefit residuals Magnetometer')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(prefit_res(2, 2, :)), 'LineWidth',1)
+ylim([-0.0000001, 0.0000001])
 xlabel('t [s]')
-ylabel('\Delta y [T]')
+ylabel('z_y [T]')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(prefit_res(3, 2, :)), 'LineWidth',1)
+ylim([-0.0000001, 0.0000001])
 xlabel('t [s]')
-ylabel('\Delta z [T]')
+ylabel('z_z [T]')
 
 %Star
 figure()
 subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(prefit_res(1, 3, :)), 'LineWidth',1)
+ylim([-0.001, 0.001])
 xlabel('t [s]')
-ylabel('\Delta x')
-title('Prefit residuals (modelled vs actual) Star Tracker (mean), (\sigma^2 = 0.001 deg)')
+ylabel('z_x')
+title('Prefit residuals Star Tracker (mean)')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(prefit_res(2, 3, :)), 'LineWidth',1)
+ylim([-0.001, 0.001])
 xlabel('t [s]')
-ylabel('\Delta y')
+ylabel('z_y')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(prefit_res(3, 3, :)), 'LineWidth',1)
+ylim([-0.001, 0.001])
 xlabel('t [s]')
-ylabel('\Delta z')
+ylabel('z_z')
 
 %% Residuals Post fit
+%Stats:
+mean_postfit_sun = mean(squeeze(postfit_res(:, 1, :)), 2)
+cov_postfit_sun = cov(squeeze(postfit_res(:, 1, :))')
+mean_postfit_mag = mean(squeeze(postfit_res(:, 2, :)), 2)
+cov_postfit_mag = cov(squeeze(postfit_res(:, 2, :))')
+mean_postfit_star = mean(squeeze(postfit_res(:, 3, :)), 2)
+cov_postfit_star = cov(squeeze(postfit_res(:, 3, :))')
+
 % Residuals
 figure()
 subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(postfit_res(1, 1, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta x')
-title('Postfit residuals (modelled vs actual) Sun Sensor (\sigma^2 = 0.5 deg)')
+ylabel('z_x')
+title('Postfit residuals Sun Sensor')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(postfit_res(2, 1, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta y')
+ylabel('z_y')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(postfit_res(3, 1, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta z')
+ylabel('z_z')
 
 %Magnet
 figure()
@@ -166,39 +191,39 @@ subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(postfit_res(1, 2, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta x [T]')
-title('Postfit residuals (modelled vs actual) Magnetometer (\sigma^2 = 4.875*10^{-7} T)')
+ylabel('z_x [T]')
+title('Postfit residuals Magnetometer')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(postfit_res(2, 2, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta y [T]')
+ylabel('z_y [T]')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(postfit_res(3, 2, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta z [T]')
+ylabel('z_z [T]')
 
 %Star
 figure()
 subplot(3,1,1)
 hold on;
 plot(t_out, squeeze(postfit_res(1, 3, :)), 'LineWidth',1)
+ylim([-5*10^(-4), 5*10^(-4)])
 xlabel('t [s]')
-ylabel('\Delta x')
-title('Postfit residuals (modelled vs actual) Star Tracker (mean), (\sigma^2 = 0.001 deg)')
+ylabel('z_x')
+title('Postfit residuals Star Tracker (mean)')
 subplot(3,1,2)
 hold on;
 plot(t_out, squeeze(postfit_res(2, 3, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta y')
+ylabel('z_y')
 subplot(3,1,3)
 hold on;
 plot(t_out, squeeze(postfit_res(3, 3, :)), 'LineWidth',1)
 xlabel('t [s]')
-ylabel('\Delta z')
+ylabel('z_z')
 %% EKF
-
 figure()
 subplot(4,1,1)
 hold on;
@@ -227,17 +252,121 @@ xlabel('t [s]')
 ylabel('q4')
 legend('Estimated Quaternions', 'True Quaternions')
 
+%% Calculate errors in quat
+DCM_estimated = zeros(3,3,length(DCM_out));
+errors_quat = zeros(size(EKF_x_post(:, 4:7)));
+for i = 4:length(DCM_out)
+    q4 = EKF_x_post(i, 7);
+    q1 = EKF_x_post(i, 4);
+    q2 = EKF_x_post(i, 5);
+    q3 = EKF_x_post(i, 6);
+     
+    DCM_estimated = quat2dcm([EKF_x_post(i, 7), EKF_x_post(i, 4:6)]);
+    DCM_error_quat = DCM_out(:,:,i) * DCM_estimated';
+    errors = dcm2quat(DCM_error_quat);
+    errors = errors./norm(errors);
+    
+    errors_quat(i, :) = [errors(2:4), errors(1)];
+end
+
+
+%% Uncertainty plots
+std_wx = squeeze(sqrt(EKF_P_post(1,1,:)));
+std_wy = squeeze(sqrt(EKF_P_post(2,2,:)));
+std_wz = squeeze(sqrt(EKF_P_post(3,3,:)));
+std_q1 = 3*squeeze(sqrt(EKF_P_post(4,4,:)));
+std_q2 = 3*squeeze(sqrt(EKF_P_post(5,5,:)));
+std_q3 = 3*squeeze(sqrt(EKF_P_post(6,6,:)));
+std_q4 = 3*squeeze(sqrt(EKF_P_post(7,7,:)));
+
+%Statistics
+mean_errors_quat = mean(errors_quat)
+cov_errors_quat = cov(errors_quat)
+errors_om = (omega_out)-(EKF_x_post(:, 1:3));
+mean_errors_om = mean(errors_om)
+cov_errors_om = cov(errors_om)
+
+
+figure()
+subplot(3,1,1)
+fill([t_out; flipud(t_out)],[errors_om(:,1) + std_wx; flipud(errors_om(:,1) - std_wx)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+
+hold on;
+plot(t_out, errors_om(:,1), 'b', 'LineWidth', 2);
+ylim([-0.1, 0.1])
+
+xlabel('t [s]')
+ylabel('\Delta \omega_x [rad/s]')
+title('Errors in Angular Velocity over time with the standard deviations from the EKF')
+subplot(3,1,2)
+fill([t_out; flipud(t_out)],[errors_om(:,2) + std_wy; flipud(errors_om(:,2) - std_wy)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_om(:,1), 'b', 'LineWidth', 2);
+ylim([-0.1, 0.1])
+
+xlabel('t [s]')
+ylabel('\Delta \omega_y [rad/s]')
+subplot(3,1,3)
+fill([t_out; flipud(t_out)],[errors_om(:,3) + std_wz; flipud(errors_om(:,3) - std_wz)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_om(:,1), 'b', 'LineWidth', 2);
+ylim([-0.1, 0.1])
+
+xlabel('t [s]')
+ylabel('\Delta \omega_z [rad/s]')
+legend('Standard Deviation', 'True Error Omega')
+
 %% errors in quat
 figure()
 subplot(4,1,1)
 hold on;
-plot(t_out(20:end-2), (quat_out(20:end-2, 1))-(EKF_x_post_min(20:end-2, 4)), 'red', 'LineWidth',1)
+fill([t_out; flipud(t_out)],[mean_errors_quat(:,1) + std_q1; flipud(mean_errors_quat(:,1) - std_q1)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_quat(:,1), 'b', 'LineWidth', 1);
+ylim([-0.0005, 0.0005])
+xlabel('t [s]')
+ylabel('\Delta q1')
+title('Errors in Quaternions over time with the 3\sigma deviations from the EKF')
+
+subplot(4,1,2)
+hold on;
+fill([t_out; flipud(t_out)],[mean_errors_quat(:,2) + std_q2; flipud(mean_errors_quat(:,2) - std_q2)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_quat(:,2), 'b', 'LineWidth', 1);
+ylim([-0.0005, 0.0005])
+xlabel('t [s]')
+ylabel('\Delta q2')
+
+subplot(4,1,3)
+hold on;
+fill([t_out; flipud(t_out)],[mean_errors_quat(:,3) + std_q3; flipud(mean_errors_quat(:,3) - std_q3)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_quat(:,3), 'b', 'LineWidth', 1);
+ylim([0.0001, 0.0009])
+xlabel('t [s]')
+ylabel('\Delta q3')
+
+subplot(4,1,4)
+hold on;
+fill([t_out; flipud(t_out)],[mean_errors_quat(:,4) + std_q4; flipud(mean_errors_quat(:,4) - std_q4)],'g', 'FaceAlpha', 1, 'FaceColor', '#8fce00', 'LineStyle', 'none');
+hold on;
+plot(t_out, errors_quat(:,4), 'b', 'LineWidth', 1);
+ylim([0.9995, 1.0005])
+xlabel('t [s]')
+ylabel('\Delta q4')
+legend('3\sigma Deviation', 'True Error Quaternion')
+
+%% errors in quat
+figure()
+subplot(4,1,1)
+hold on;
+plot(t_out, (quat_out)-(EKF_x_post(:, 4)), 'red', 'LineWidth',1)
 xlabel('t [s]')
 ylabel('\Delta q1')
 title('Quaternion Error between EKF estimated and true (with perturbations')
 subplot(4,1,2)
 hold on;
-plot(t_out(20:end-2), (quat_out(20:end-2, 2))-(EKF_x_post_min(20:end-2, 5)), 'red',  'LineWidth',1)
+plot(t_out, (quat_out)-(EKF_x_post(:, 4)), 'red', 'LineWidth',1)
 xlabel('t [s]')
 ylabel('\Delta q2')
 subplot(4,1,3)
@@ -276,7 +405,29 @@ xlabel('t [s]')
 ylabel('\Delta q4')
 
 
-
+%%
+figure()
+subplot(4,1,1)
+hold on;
+plot(t_out, EKF_x_post(:, 4) - EKF_x_post_min(:, 4), 'red', 'LineWidth',1)
+xlabel('t [s]')
+ylabel('\Delta q1')
+title('Quaternion Error between EKF prior and posterior')
+subplot(4,1,2)
+hold on;
+plot(t_out, EKF_x_post(:, 5) - EKF_x_post_min(:, 5), 'red', 'LineWidth',1)
+xlabel('t [s]')
+ylabel('\Delta q2')
+subplot(4,1,3)
+hold on;
+plot(t_out, EKF_x_post(:, 6) - EKF_x_post_min(:, 6), 'red', 'LineWidth',1)
+xlabel('t [s]')
+ylabel('\Delta q3')
+subplot(4,1,4)
+hold on;
+plot(t_out, EKF_x_post(:, 7) - EKF_x_post_min(:, 7), 'red', 'LineWidth',1)
+xlabel('t [s]')
+ylabel('\Delta q4')
 %% errors in omega
 figure()
 subplot(3,1,1)
@@ -323,13 +474,13 @@ xlabel('t [s]')
 ylabel('\omega_z [rad/s]')
 legend('Estimated Omega', 'True Omega')
 %% Uncertainty plots
-std_wx = squeeze(sqrt(EKF_P_post_min(1,1,:)));
-std_wy = squeeze(sqrt(EKF_P_post_min(2,2,:)));
-std_wz = squeeze(sqrt(EKF_P_post_min(3,3,:)));
-std_q1 = squeeze(sqrt(EKF_P_post_min(4,4,:)));
-std_q2 = squeeze(sqrt(EKF_P_post_min(5,5,:)));
-std_q3 = squeeze(sqrt(EKF_P_post_min(6,6,:)));
-std_q4 = squeeze(sqrt(EKF_P_post_min(7,7,:)));
+std_wx = squeeze(sqrt(EKF_P_post(1,1,:)));
+std_wy = squeeze(sqrt(EKF_P_post(2,2,:)));
+std_wz = squeeze(sqrt(EKF_P_post(3,3,:)));
+std_q1 = squeeze(sqrt(EKF_P_post(4,4,:)));
+std_q2 = squeeze(sqrt(EKF_P_post(5,5,:)));
+std_q3 = squeeze(sqrt(EKF_P_post(6,6,:)));
+std_q4 = squeeze(sqrt(EKF_P_post(7,7,:)));
 
 
 figure()
